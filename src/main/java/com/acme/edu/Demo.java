de5bf44fb55710c0323763900a416e83e20977f7
Alena <sharko_alena@mail.ru>
2015-10-28 17:02:31 +0300

    add code to test maven

---
+++ src/main/java/com/acme/edu/Demo.java
@@ -0,0 +1,8 @@
+
+package com.acme.edu;
+    public class Demo{
+        public static void main (String[] args){
+            com.acme.edu.Printer.print("Hello");
+        }
+    }
+
---
+++ src/main/java/com/acme/edu/Printer.java
@@ -0,0 +1,6 @@
+package com.acme.edu;
+class Printer{
+    public static void print (String s){
+        System.out.println(s);
+    }
+}
