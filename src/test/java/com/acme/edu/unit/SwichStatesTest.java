6b7deb76bf6bb3a8a6864748cc9ea513ec0f35b6
Alena <sharko_alena@mail.ru>
2015-11-04 23:20:53 +0300

    Make project OOP Iteraton 1 - all tests Iteration 2 - all tests Iteration 3 - all tests bug fix - without static
    Tests for Integer State
    Tests for String State
    Tests for Default State
    Test for chenging states

---
+++ src/test/java/com/acme/edu/unit/SwichStatesTest.java
@@ -0,0 +1,48 @@
+package com.acme.edu.unit;
+
+import com.acme.edu.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for chenging states
+ */
+public class SwichStatesTest {
+
+
+    Printer printer;
+
+    @Before
+    public void setUp() {
+        printer = mock(ConsolPrinter.class);
+    }
+
+    @Test
+    public void ShouldReturnIntStateWhenBeforeWasStringState() {
+        StateString stub = mock(StateString.class);
+        when(stub.swichStateToStringState()).thenReturn(new StateInt(printer));
+    }
+
+    @Test
+    public void ShouldReturnStringStateWhenBeforeWasStringState() {
+        StateString stub = mock(StateString.class);
+        when(stub.swichStateToIntState()).thenReturn(new StateString(printer));
+    }
+
+    @Test
+    public void ShouldReturnStringStateWhenBeforeWasIntState() {
+        StateInt stub = mock(StateInt.class);
+        when(stub.swichStateToStringState()).thenReturn(new StateInt(printer));
+    }
+
+    @Test
+    public void ShouldReturnIntStateWhenBeforeWasStringInt() {
+        StateInt stub = mock(StateInt.class);
+        when(stub.swichStateToIntState()).thenReturn(new StateInt(printer));
+    }
+
+
+}
