a5c352faed850d93bdf18b1c3a51ff0827c0fd4a
Alena <sharko_alena@mail.ru>
2015-11-06 14:08:27 +0300

    find lost printer

---
+++ src/main/java/com/acme/edu/Printers/PrintFile.java
@@ -0,0 +1,7 @@
+package com.acme.edu.Printers;
+
+/**
+ * Created by alena on 06.11.15.
+ */
+public class PrintFile {
+}
--- src/main/java/com/acme/edu/Printers/Printer.java
+++ src/main/java/com/acme/edu/Printers/Printer.java
@@ -12,16 +12,5 @@ public interface Printer {
      */
     void print(String message);
 
-    /**
-     * Print message in the console
-     *
-     * @author Alena Sharko
-     */
-    class ConsolPrinter implements Printer {
 
-        @Override
-        public void print(String message) {
-            System.out.println(message);
-        }
-    }
 }
