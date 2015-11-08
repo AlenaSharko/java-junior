ddf8dd880354bddb2580972da4b3a6e40cc9888a
Alena <sharko_alena@mail.ru>
2015-11-08 21:34:47 +0300

    File test printer for exeption

--- src/test/java/com/acme/edu/unit/printerTests/FilePrinterTest.java
+++ src/test/java/com/acme/edu/unit/printerTests/FilePrinterTest.java
@@ -1,14 +1,14 @@
 package com.acme.edu.unit.printerTests;
 
 import com.acme.edu.exeptions.PrinterExeption;
-import com.acme.edu.logger.Logger;
 import com.acme.edu.printers.FilePrinter;
 import com.acme.edu.printers.Printer;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -24,7 +24,7 @@ public class FilePrinterTest {
     @Before
     public void setUp() throws PrinterExeption {
         testFile = new File("testFile.txt");
-        printer = new FilePrinter(testFile.getPath(), Charset.defaultCharset());
+
     }
 
     @After
@@ -33,11 +33,18 @@ public class FilePrinterTest {
     }
 
     @Test
-    public void shouldPrintMessageInFile() throws PrinterExeption , IOException{
-
+    public void shouldPrintMessageInFile() throws PrinterExeption, IOException {
+        printer = new FilePrinter(testFile.getPath(), Charset.defaultCharset());
         printer.print("test string");
 
         Assert.assertEquals("test string", FileUtils.readFileToString(testFile));
+    }
+
+    @Test(expected = PrinterExeption.class)
+
+    public void shouldntPrintMessageInFile() throws PrinterExeption, IOException {
+        printer = new FilePrinter("///invalid file path///", Charset.defaultCharset());
+        printer.print("test string");
     }
 
 }
